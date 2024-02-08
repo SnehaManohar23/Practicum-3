fd324c8926d8832413e215bbc15c0231b9829e32
Sneha Manoharan <snehamanoharan@Snehas-MacBook-Air.local>
2024-02-08 17:56:40 -0500

    modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const cors=require('cors');
+const cors = require('cors');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 app.use(cors());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 
 app.get('/add', (req, res) => {
     const num1 = parseFloat(req.query.num1);
